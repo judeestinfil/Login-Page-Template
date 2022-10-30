14ced3de49d9681eb502ef77033cd2bd766aa2b2
judeestinfil <judeestinfil9@gmail.com>
2022-10-29 20:54:50 -0400

    added socials

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(bodyparser.urlencoded({extended:true}))
 app.set('view engine', 'ejs');
 
 //load static assets
-app.use('/static',express.static(path.join(__dirname,'public')))
-app.use('/assets',express.static(path.join(__dirname,'public/assets')))
+app.use(express.static( 'public'))
+
 
 app.use(session({
     secret:uuidv4(),// '1b9d6bcd-bbfd-4b2d-9b5d-ab985904nlkv'
